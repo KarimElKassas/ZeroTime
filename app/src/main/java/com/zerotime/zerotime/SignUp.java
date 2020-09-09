924b59ea2f8b9c235d3759928b7c443c86b965b0
El Kassas <elkassas2022@gmail.com>
2020-09-09 15:03:26 +0200

    sign up Activity design

---
+++ app/src/main/java/com/zerotime/zerotime/SignUp.java
@@ -0,0 +1,22 @@
+package com.zerotime.zerotime;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.zerotime.zerotime.databinding.ActivitySignUpBinding;
+
+public class SignUp extends AppCompatActivity {
+
+    private ActivitySignUpBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+    }
+}
