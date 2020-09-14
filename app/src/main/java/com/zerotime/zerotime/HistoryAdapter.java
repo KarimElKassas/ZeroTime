75860e18ee44169b8afc44cc33df2654fde911b6
El Kassas <elkassas2022@gmail.com>
2020-09-14 17:34:03 +0200

    Merge branch 'master' of https://github.com/KarimElKassas/ZeroTime into master
     Conflicts:
    	app/src/main/java/com/zerotime/MessageAdapter.java
    	app/src/main/java/com/zerotime/zerotime/Adapters/MessageAdapter.java
    	app/src/main/java/com/zerotime/zerotime/Message.java
    	app/src/main/java/com/zerotime/zerotime/MessageAdapter.java

---
+++ app/src/main/java/com/zerotime/zerotime/HistoryAdapter.java
@@ -0,0 +1,70 @@
+package com.zerotime.zerotime;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class HistoryAdapter extends RecyclerView.Adapter<HistoryAdapter.HistoryViewHolder> {
+    private List<HistoryPojo> historyList = new ArrayList<>();
+    Context context;
+
+    public HistoryAdapter(List<HistoryPojo> historyList, Context context) {
+        this.historyList = historyList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public HistoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new HistoryViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.history_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HistoryViewHolder holder, int position) {
+        HistoryPojo history = historyList.get(position);
+        holder.description.setText(history.getDescription());
+        holder.date.setText(history.getDate());
+        holder.price.setText(history.getPrice());
+        holder.size.setText(history.getSize());
+        holder.Raddress.setText(history.getRaddress());
+        holder.Rname.setText(history.getRname());
+        holder.Rphone1.setText(history.getRphone1());
+        holder.Rphone2.setText(history.getRphone2());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return historyList.size();
+    }
+
+    public void setList(List<HistoryPojo> historyList) {
+        this.historyList = historyList;
+        notifyDataSetChanged();
+    }
+
+    public class HistoryViewHolder extends RecyclerView.ViewHolder {
+        TextView description, date, price, size, Raddress, Rname, Rphone1, Rphone2;
+
+        public HistoryViewHolder(@NonNull View itemView) {
+            super(itemView);
+            description = itemView.findViewById(R.id.history_description);
+            date = itemView.findViewById(R.id.history_date);
+            price = itemView.findViewById(R.id.history_price);
+            size = itemView.findViewById(R.id.history_size);
+            Raddress = itemView.findViewById(R.id.history_RAddress);
+            Rname = itemView.findViewById(R.id.history_RName);
+            Rphone1 = itemView.findViewById(R.id.history_RPhone1);
+            Rphone2 = itemView.findViewById(R.id.history_RPhone2);
+        }
+    }
+}
---
+++ app/src/main/java/com/zerotime/zerotime/MessageAdapter.java
@@ -0,0 +1,75 @@
+package com.zerotime.zerotime;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+import com.zerotime.zerotime.Secretary.Pojos.Chat;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageViewHolder> {
+    public static final int msgTypeLeft = 0;
+    public static final int msgTypeRight = 1;
+    Context context;
+    private List<Chat> Chats;
+
+    public MessageAdapter(Context context, List<Chat> Chats) {
+        this.context = context;
+        this.Chats = Chats;
+    }
+
+    @NonNull
+    @Override
+    public MessageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (viewType == msgTypeRight){
+            View ItemView = LayoutInflater.from(context).inflate(R.layout.item_chat_right,parent,false);
+            return new MessageViewHolder(ItemView);
+        }
+        else{
+            View ItemView = LayoutInflater.from(context).inflate(R.layout.item_chat_left,parent,false);
+            return new MessageViewHolder(ItemView);
+        }
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MessageViewHolder holder, int position) {
+        Chat mchat = Chats.get(position);
+        holder.showMessage.setText(mchat.getMessage());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return Chats.size();
+    }
+
+    public void setList(List<Chat> moviesList) {
+        this.Chats = moviesList;
+        notifyDataSetChanged();
+    }
+
+    public class MessageViewHolder extends RecyclerView.ViewHolder {
+        TextView showMessage;
+
+        public MessageViewHolder(@NonNull View itemView) {
+            super(itemView);
+            showMessage = itemView.findViewById(R.id.showMessage);
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (Chats.get(position).getSender().equals("Zero Time")) {
+            return msgTypeRight;
+        } else return msgTypeLeft;
+
+    }
+}
