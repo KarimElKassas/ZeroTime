5468049c1dd532e96a0b0a6e8f3a300f1c0d0f0c
El Kassas <elkassas2022@gmail.com>
2020-09-14 17:33:18 +0200

    Merge branch 'master' of https://github.com/KarimElKassas/ZeroTime into master
     Conflicts:
    	app/src/main/java/com/zerotime/MessageAdapter.java
    	app/src/main/java/com/zerotime/zerotime/Adapters/MessageAdapter.java
    	app/src/main/java/com/zerotime/zerotime/Message.java
    	app/src/main/java/com/zerotime/zerotime/MessageAdapter.java

--- app/src/main/java/com/zerotime/zerotime/Message.java
+++ app/src/main/java/com/zerotime/zerotime/Message.java
@@ -14,7 +14,6 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.zerotime.zerotime.Adapters.MessageAdapter;
 import com.zerotime.zerotime.Secretary.Pojos.Chat;
 import com.zerotime.zerotime.databinding.ActivityMessageBinding;
 
